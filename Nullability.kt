08820b33495f2a5090f227d5b06a5ce09d789884
Clinton Ogada <clintonogada@gmail.com>
2023-03-01 18:51:46 +0300

    Create Nullability.kt
    To declare nullable variables in Kotlin, you need to add a ? operator to the end of the type. For example, a String? type can hold either a string or null, whereas a String type can only hold a string. To declare a nullable variable, you need to explicitly add the nullable type. Without the nullable type, the Kotlin compiler infers that it's a non-nullable type

---
+++ Nullability.kt
@@ -0,0 +1,9 @@
+fun main() {
+
+    /*This program prints the favoriteActor variable before and after the null reassignment,*/
+    var favoriteActor: String? = "Sandra Oh"
+    println(favoriteActor)
+
+    favoriteActor = null
+    println(favoriteActor)
+}
